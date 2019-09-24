6e863cfc346cbb78d2fe2a2f67db05dead12590d
ced-ashutoshv <ashutoshverma@cedcoss.com>
2019-09-24 11:32:33 +0530

    elementor issue resolved

--- includes/class-woocommerce-one-click-upsell-funnel-global_functions.php
+++ includes/class-woocommerce-one-click-upsell-funnel-global_functions.php
@@ -18,7 +18,7 @@
  */
 function mwb_upsell_lite_elementor_plugin_active() {
 
-	if ( ( ! in_array( 'elementor/elementor.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) ) {
+	if ( ( in_array( 'elementor/elementor.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) ) {
 		
 		return true;
 	} else {
