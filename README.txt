749f976510e9603926fdf3d6306410d1202e97c9
ananya-cedcoss <ananyashukla@makewebbetter.com>
2022-10-14 13:40:08 +0530

    version update

--- README.txt
+++ README.txt
@@ -6,7 +6,7 @@ Requires at least: 5.1.0
 Tested up to: 6.0.1
 WC requires at least: 5.1.0
 WC tested up to: 6.8.2
-Stable tag: 3.2.2
+Stable tag: 3.2.3
 Requires PHP: 7.0.0
 License: GPLv3 or later 
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
@@ -255,7 +255,7 @@ Most of the major WordPress page builders like Elementor, WPBakery, Divibuilders
 
 == Changelog ==
 
-= 3.2.2 - Released on 23 August 2022 =
+= 3.2.3 - Released on 23 August 2022 =
 * New: Minor bug fixes
 * New: Compatible with the latest WP and WC
 
@@ -354,7 +354,7 @@ Most of the major WordPress page builders like Elementor, WPBakery, Divibuilders
 
 == Upgrade Notice ==
 
-= 3.2.2 - Released on 23 August 2022 =
+= 3.2.3 - Released on 23 August 2022 =
 * New: Minor bug fixes
 * New: Compatible with latest WP and WC
 
--- woocommerce-one-click-upsell-funnel.php
+++ woocommerce-one-click-upsell-funnel.php
@@ -15,7 +15,7 @@
  * Plugin Name:           One Click Upsell Funnel for Woocommerce
  * Plugin URI:            https://wordpress.org/plugins/woo-one-click-upsell-funnel/
  * Description:           One Click Upsell Funnel for WooCommerce allows showing post-checkout offers to customers which helps to increase Average Order Value & Revenue. <a href="https://wpswings.com/woocommerce-plugins/?utm_source=wpswings-upsell-shop&utm_medium=upsell-org-backend&utm_campaign=shop-page" target="_blank" >Elevate your e-commerce store by exploring more on <strong>WP Swings</strong></a>.
- * Version:               3.2.2
+ * Version:               3.2.3
  *
  * Requires at least:     5.1.0
  * Tested up to:          6.0.1
@@ -199,7 +199,7 @@ if ( true === $wps_upsell_lite_plugin_activation['status'] ) {
 
 		define( 'WPS_WOCUF_DIRPATH', plugin_dir_path( __FILE__ ) );
 
-		define( 'WPS_WOCUF_VERSION', 'v3.2.2' );
+		define( 'WPS_WOCUF_VERSION', 'v3.2.3' );
 
 		/**
 		 * The code that runs during plugin activation.
