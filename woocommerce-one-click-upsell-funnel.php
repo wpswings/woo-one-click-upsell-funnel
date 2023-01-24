a9469b09129a4a4f6143d2ba0a425dad072a6c95
ananya-cedcoss <ananyashukla@makewebbetter.com>
2023-01-24 12:17:14 +0530

    upsell shipping

--- woocommerce-one-click-upsell-funnel.php
+++ woocommerce-one-click-upsell-funnel.php
@@ -15,12 +15,12 @@
  * Plugin Name:           One Click Upsell Funnel for Woocommerce
  * Plugin URI:            https://wordpress.org/plugins/woo-one-click-upsell-funnel/
  * Description:           One Click Upsell Funnel for WooCommerce allows showing post-checkout offers to customers which helps to increase Average Order Value & Revenue. <a href="https://wpswings.com/woocommerce-plugins/?utm_source=wpswings-upsell-shop&utm_medium=upsell-org-backend&utm_campaign=shop-page" target="_blank" >Elevate your e-commerce store by exploring more on <strong>WP Swings</strong></a>.
- * Version:               3.2.4
+ * Version:               3.2.5
  *
  * Requires at least:     5.1.0
  * Tested up to:          6.1.1
  * WC requires at least:  5.1.0
- * WC tested up to:       7.2.0
+ * WC tested up to:       7.3.0
  *
  * Author:                WP Swings
  * Author URI:            https://wpswings.com/?utm_source=wpswings-official&utm_medium=upsell-org-backend&utm_campaign=official
@@ -199,7 +199,7 @@ if ( true === $wps_upsell_lite_plugin_activation['status'] ) {
 
 		define( 'WPS_WOCUF_DIRPATH', plugin_dir_path( __FILE__ ) );
 
-		define( 'WPS_WOCUF_VERSION', 'v3.2.4' );
+		define( 'WPS_WOCUF_VERSION', 'v3.2.5' );
 
 		/**
 		 * The code that runs during plugin activation.
