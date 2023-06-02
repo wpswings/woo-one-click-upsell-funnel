1633e36b73f732ee14729798696a38044446352c
ananya-cedcoss <ananyashukla@makewebbetter.com>
2023-06-02 15:45:17 +0530

    version 3.2.8

--- woocommerce-one-click-upsell-funnel.php
+++ woocommerce-one-click-upsell-funnel.php
@@ -18,9 +18,9 @@
  * Version:               3.2.8
  *
  * Requires at least:     5.5.0
- * Tested up to:          6.2.0
+ * Tested up to:          6.2.2
  * WC requires at least:  5.5.0
- * WC tested up to:       7.6.1
+ * WC tested up to:       7.7.2
  *
  * Author:                WP Swings
  * Author URI:            https://wpswings.com/?utm_source=wpswings-official&utm_medium=upsell-org-backend&utm_campaign=official
