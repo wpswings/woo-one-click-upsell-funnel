aed44b5ec030e6cebacdd688f92f580ec8b4b3b7
ananya-cedcoss <ananyashukla@makewebbetter.com>
2023-10-30 16:37:07 +0530

    phpcs issues fixed

--- woocommerce-one-click-upsell-funnel.php
+++ woocommerce-one-click-upsell-funnel.php
@@ -20,7 +20,7 @@
  * Requires at least:     5.5.0
  * Tested up to:          6.3.2
  * WC requires at least:  5.5.0
- * WC tested up to:       8.2.0
+ * WC tested up to:       8.2.1
  *
  * Author:                WP Swings
  * Author URI:            https://wpswings.com/?utm_source=wpswings-official&utm_medium=upsell-org-backend&utm_campaign=official
