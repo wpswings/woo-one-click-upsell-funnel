eea91ca88150c2e368aa68660f2f1dc60179d788
ananya-cedcoss <ananyashukla@makewebbetter.com>
2023-11-28 10:52:52 +0530

    issues fixed

--- includes/class-woocommerce-one-click-upsell-funnel.php
+++ includes/class-woocommerce-one-click-upsell-funnel.php
@@ -415,7 +415,7 @@ add_filter( 'woocommerce_get_checkout_order_received_url', 'wps_wocuf_redirect_o
  * Function to save redirection.
  *
  * @param string    $order_received_url is the order url.
- * @param [type] $data is the order data.
+ * @param object $data is the order data.
  * @return string
  */
 function wps_wocuf_redirect_order_while_upsell_org( $order_received_url, $data ) {
