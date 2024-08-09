fb4f1aa2f70bdbdb0356424f7a51c6b9c73b0144
ananya-cedcoss <ananyashukla@makewebbetter.com>
2024-08-09 12:37:14 +0530

    require woocommerce added

--- woocommerce-one-click-upsell-funnel.php
+++ woocommerce-one-click-upsell-funnel.php
@@ -17,6 +17,7 @@
  * Description:           One Click Upsell Funnel for WooCommerce allows showing post-checkout offers to customers which helps to increase Average Order Value & Revenue. <a href="https://wpswings.com/woocommerce-plugins/?utm_source=wpswings-upsell-shop&utm_medium=upsell-org-backend&utm_campaign=shop-page" target="_blank" >Elevate your e-commerce store by exploring more on <strong>WP Swings</strong></a>.
  * Version:               3.4.7
  *
+ * Requires Plugins: woocommerce
  * Requires at least:     5.5.0
  * Tested up to:          6.6.1
  * WC requires at least:  6.5.0
